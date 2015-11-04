ae2793a94c632dde9ba01300f1b7cb4abeb77dbb
frances harlow <frances.harlow@gmail.com>
2015-11-04 16:35:04 -0500

    progress on act 2

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 var promptBox = document.getElementById('prompt');
 var log = document.getElementById('log');
 var purse = 10000;
+var ledger = {};
 
 var pullOutPurse = function(){
   var purseTotalLi = document.createElement('li');
@@ -13,23 +14,45 @@ var pullOutPurse = function(){
   log.appendChild(purseTotalLi);
 }
 
+var lendMoney = function(){
+  var command = promptBox.value;
+  var commandArray = command.split(" ");
+  ledger.action = commandArray[0];
+  ledger.lender = commandArray[1];
+  ledger.amount = commandArray[2];
+  var lendMoneyLi = document.createElement('li');
+  lendMoneyLi.textContent = 'ShylockBot gives ' + commandArray[1] + ' ' + commandArray[2] + ' ducats';
+  lendMoneyLi.classList.add('action');
+  log.appendChild(lendMoneyLi);
+}
+
+var trackLoans = function(){
+  purseTotalLi.classList.add('action');
+  purse -= Ledger.amount;
+}
+
 // var collectInterest = function(){}
 
 // var collectDebts = function(){}
 
-
-
 var addToLog = function(event){
+
   var logLiCommand = document.createElement('li');
+  var command = promptBox.value;
+  var commandArray = command.split(" ");
   logLiCommand.textContent = promptBox.value
+  logLiCommand.classList.add('command');
   log.appendChild(logLiCommand);
     if (promptBox.value === 'purse') {    
       pullOutPurse();
-    } else if (promptBox.value === 'ledger') {
-
+    } else if (commandArray[0] === 'lend') {
+      lendMoney();
+      trackLoans();
     }
 
-    // else if (promptBox.value === 'collect interest') {
+    // } else if (promptBox.value === 'ledger') {
+    //   trackLoans();
+    // } else if (promptBox.value === 'collect interest') {
     //   collectInterest(); {
     // } else if (prompt.Box.value === 'collect') {
     //   collectDebts();
